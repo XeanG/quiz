c07bd41e3bdfc2d3e4e0edffe1ac8d74019aad91
andre <abastilhossouza@gmail.com>
2021-07-01 23:13:15 -0300

    o coisa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-/*
-
-NAO TEM NADA EH SÓ PRA PREEENCHER O ARQUIVO
-
-*/
+function test() {
+  var name = document.getElementById("name").value;
+  if (name == "itsmymom") {
+    alert("você venceu. Próximo nível: voce_é_idiota.html | lmth.atoidi_é_ecov :levín omixórP .uecnev êcov");
+  } else {
+    alert("Try again | niaga yrT")
+  }
+}
